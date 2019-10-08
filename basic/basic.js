7778ca701b7457d955b04cabcbe5ba8742cadd1b
calamanshi <calamanshi@gmx.at>
2019-10-08 11:26:48 +0200

    m

---
+++ basic/basic.js
@@ -0,0 +1,18 @@
+$(".bell").on("click", function(){
+alert("you clicked the bell")
+});
+$(".santa").on("click", function(){
+alert("you clicked the santa")
+});
+$(".gift").on("click", function(){
+alert("you clicked the gift")
+});
+$(".santa").on("click", function(){
+$(".santa").css("opacity", "0");
+});
+
+// $("div.note").css({"color":"yellow", "background-color":"black"});
+
+// $("div:contains(gift)").on("click", function(){
+// alert("you clicked the santa")
+// });
---
+++ intermediate/challenge.js
@@ -0,0 +1,16 @@
+    // dave has this line
+    // var data = Jason(products);
+
+    for (i = 0; i < 6; i++) {
+        document.writeln("<td>");
+        document.writeln("<table border='1' width=100 >");
+        document.writeln("<tr><td><b>Unique ID</b></td><td width=50>" + products[i].UniqueID+ "</td></tr>");
+        document.writeln("<tr><td><b>Product Name</b></td><td width=50>" + products[i].productName+ "</td></tr>");
+        document.writeln("<tr><td><b>Price</b></td><td width=50>" + products[i].price+ "</td></tr>");
+        document.writeln("<tr><td><b>Description</b></td><td width=50>" + products[i].description+"</td></tr>");
+        document.writeln("<tr><td><b>Last Name</b></td><td width=50>" + products[i].img+ "</td></tr>");
+        document.writeln("<tr><td><b>do buy</b></td><td width=50><button type='button' class='buypr"+i+"' onclick='addToCart"+i+"()''>buy me"+i+"</button></td></tr>");
+        
+        document.writeln("</table>");
+        document.writeln("</td>");
+    }
