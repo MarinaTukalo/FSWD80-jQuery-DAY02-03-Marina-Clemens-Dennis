290d4123a45eb63ce8c629b414077eccbb27cb4a
Dennis Pavitschitz <d.pavitschitz@hotmail.com>
2019-10-09 13:00:20 +0200

    intermediateEx part1

---
+++ intermediate-day2-den/inter.js
@@ -0,0 +1,23 @@
+			$(document).ready(function () {
+			    $(".notebook").on({
+
+			        click: function () {
+			            $('body').css("background-color", "red");
+
+			        }
+			    });
+			    $(".phone").on({
+
+			        click: function () {
+			            $('body').css("background-color", "green");
+
+			        }
+			    });
+			    $(".tablet").on({
+
+			        click: function () {
+			            $('body').css("background-color", "blue");
+
+			        }
+			    });
+			});
